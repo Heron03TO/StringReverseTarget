9eab9ece820e1bf5857d913bff193844867142d7
Heron Tolentino <heron.tolentino97@gmail.com>
2023-02-27 21:10:15 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string inputString = "Eu vou conseguir a aprovação, se tudo der certo";
+string reversedString = "";
+
+for (int i = inputString.Length - 1; i >= 0; i--)
+{
+    reversedString += inputString[i];
+}
+
+Console.WriteLine(reversedString);
+
